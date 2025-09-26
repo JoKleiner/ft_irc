f4c17708035eca890faa1e71c8d9ecf6475ebd1d
joklein <joklein@student.42heilbronn.de>
2025-09-26 17:04:39 +0200

    include Sigpipe ignore

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,10 @@ int main(int argc, char **argv)
 	if (argc != 3)
 		return (std::cerr << "Error: starting server: './ircserv <port> <password>'" << std::endl, 1);
 
+	
 	if (!std::regex_match(argv[1], std::regex("(\\d{4}|\\d{5})")))
 		return (std::cerr << "Error: Port should be in between 1024-49151" << std::endl, 1);
-
+	signal(SIGPIPE, SIG_IGN);
 	try
 	{
 		Server::start(argv);
